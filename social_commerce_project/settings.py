8601f0d8e7c61c931e6da459818d32bb95607755
Felix D <flx.descoteaux@gmail.com>
2015-02-06 14:40:49 -0500

    fixed settings.py

--- social_commerce_project/settings.py
+++ social_commerce_project/settings.py
@@ -15,7 +15,7 @@ from django.core.exceptions import ImproperlyConfigured
 def get_env_variable(var_name):
     """Get the environment variable or return an exception"""
     try:
-        return os.environ("SOCIAL_COMMERCE_SECRET_KEY")
+        return os.environ[var_name]
     except KeyError:
         error_msg = "Set the {} environment variable".format(var_name)
         raise ImproperlyConfigured(error_msg)
