9fa2f5dfb2bab3f92f35e3537feab9b7cd0ec0f7
Felix D <flx.descoteaux@gmail.com>
2015-02-07 17:54:17 -0500

    added logout

--- social_commerce_project/urls.py
+++ social_commerce_project/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(phase1/|$)', include('phase1.urls')),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
 )
