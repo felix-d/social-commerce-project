df4042c54830c8d7fe7cf13a400ab6780fc1fdf7
Felix D <flx.descoteaux@gmail.com>
2015-02-07 14:48:41 -0500

    changed Users app to use django default user and session models

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -2,63 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Friendship',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('friend', models.ForeignKey(related_name='the_friend', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(related_name='the_user', to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Session',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('destroyed_at', models.DateTimeField()),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='User',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('oauth_uid', models.CharField(max_length=255)),
-                ('oauth_provider', models.CharField(max_length=255)),
-                ('username', models.CharField(max_length=255)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='session',
-            name='user',
-            field=models.ForeignKey(to='users.User'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='friendship',
-            name='friend',
-            field=models.ForeignKey(to='users.User', related_name='the_friend'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='friendship',
-            name='user',
-            field=models.ForeignKey(to='users.User', related_name='the_user'),
-            preserve_default=True,
         ),
     ]
