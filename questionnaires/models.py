e9f036797ab0ab92bfc975e7452cc3eb24320f0e
Felix D <flx.descoteaux@gmail.com>
2015-02-09 20:18:18 -0500

    added many to many relatship to question-questionchoice

--- questionnaires/models.py
+++ questionnaires/models.py
@@ -3,19 +3,30 @@ from django.db import models
 class Questionnaire(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class QuestionGroup(models.Model):
     name = models.CharField(max_length=255)
     questionnaire = models.ForeignKey(Questionnaire)
 
+    def __str__(self):
+        return self.name
+
+
+class QuestionChoice(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
     text = models.TextField()
     qtype = models.SmallIntegerField()
     qgroup  = models.ForeignKey(QuestionGroup)
+    qchoices = models.ManyToManyField(QuestionChoice)
 
-class QuestionChoice(models.Model):
-    name = models.CharField(max_length=255)
-    question = models.ForeignKey(Question)
-
-
-
+    def __str__(self):
+        return self.text
